70047ae55cbd5110d7e12b8f87e3ce0b9fa165a5
Alina Darenskikh <alinadarenskikh@gmail.com>
2023-02-10 15:09:40 +0300

    Add DemoOOP

---
+++ src/main/java/by/itacademy/alinadarenskikh/javabasics/oop/DemoOOP.java
@@ -0,0 +1,59 @@
+package by.itacademy.alinadarenskikh.javabasics.oop;
+
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
+
+public class DemoOOP {
+    public static void main(String[] args) {
+
+        Faker fake = new Faker();
+        Customer customer1 = new Customer();
+
+        customer1.setId(fake.idNumber().hashCode());
+        customer1.setName(fake.name().firstName());
+        customer1.setLastName(fake.name().lastName());
+        customer1.setFathersName("Mamontovich");
+
+        String add = new String();
+        add = fake.address().fullAddress();
+        customer1.setAddress(add);
+
+        customer1.setCardNumber(fake.business().creditCardNumber().hashCode());
+        customer1.setBankAccount(fake.finance().iban());
+
+        System.out.println(customer1.getName());
+        System.out.println(customer1.getLastName());
+        System.out.println(customer1.getFathersName());
+        System.out.println(customer1.getId());
+        System.out.println(customer1.getAddress());
+        System.out.println(customer1.getCardNumber());
+        System.out.println(customer1.getBankAccount());
+
+
+        Book book1 = new Book();
+        book1.setName("Изучаем Java");
+        book1.setAuthor("Берт Бейтс, Кэти Сьерра");
+        book1.setPublicationYear(2015);
+        book1.setPages(720);
+        book1.setPrice(54.45);
+        book1.setId(978-5-699-54574-2);
+
+        System.out.println("Название: " + book1.getName());
+        System.out.println("Автор: " + book1.getAuthor());
+        System.out.println("Год издания: " + book1.getPublicationYear());
+        System.out.println("Количество страниц: " + book1.getPages());
+        System.out.println("Цена: " + book1.getPrice());
+        System.out.println("ID " + book1.getId());
+
+
+        Car car1 = new Car();
+        car1.setBrand("Dodge");
+        car1.setModel("Challenger III");
+        car1.setColor("black");
+        car1.setYearOfProduction(2018);
+        car1.setPrice(110_190.0);
+        car1.setLicensePlate("AAA555");
+        car1.setId(651465);
+    }
+}
